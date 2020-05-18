84269ae680020db4a6c9283cfdaddb2fa79a999a
shenjiawei <shenjiawei@umxwe.com>
2020-05-18 14:47:26 +0800

    解决冲突2222

--- src/main/java/com/umx/eureka_server/EurekaServerApplication.java
+++ src/main/java/com/umx/eureka_server/EurekaServerApplication.java
@@ -7,12 +7,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
-
-<<<<<<< HEAD
+    
     //初始化
-=======
-    //4325324
->>>>>>> 72a2a0c4c60f8705a6fb4b58d5c021cb705a30ca
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
